0b7af8563be30699fcf7091351ff8cbac25ad3dd
swaatss <shebajaen@gmail.com>
2026-05-04 01:02:22 -0400

    remove duplicate css

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./login.css";
+import "./Login.css";
 
 const Login = ({ onLogin }) => {
   const [isRegister, setIsRegister] = useState(false);
