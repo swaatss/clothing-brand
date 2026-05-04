64fb765f9ac7c67fc890a04b644bd9af0c2d4cc7
swaatss <shebajaen@gmail.com>
2026-05-04 00:52:48 -0400

    Caps fix

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Login.css';
+import "./login.css";
 
 const Login = ({ onLogin }) => {
   const [isRegister, setIsRegister] = useState(false);
